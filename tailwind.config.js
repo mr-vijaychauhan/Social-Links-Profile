0a3f2663ca461818f4b5a40f7e99a8af68b2a8d2
VIJAY CHAUHAN <mr.vijaychauhan08@gmail.com>
2024-02-02 22:28:37 +0530

    Initial Setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        'grey':'hsl(0, 0%, 20%)',
+        'dark-grey' : 'hsl(0, 0%, 12%)',
+        'off-black' : 'hsl(0, 0%, 8%)',
+        'primary' : 'hsl(75, 94%, 57%)',
       },
+    },
+    screens:{
+      'sm': '375px',
+      'lg': '1440px'
     },
   },
   plugins: [],
